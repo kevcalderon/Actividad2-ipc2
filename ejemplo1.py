24d1372d8a9e95b29dff200e17c4dc3343195363
Kevin Calderon <josue_29c@hotmail.com>
2021-02-06 23:05:08 -0600

    [UPDATE] ejemplo1

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,5 +1,6 @@
 from xml.dom import minidom
 
+#Accedemos al archivo por medio del nombre
 doc = minidom.parse("pruebas.xml")
 
 # Por medio del nombre de las etiquetas se busca en el xml.
@@ -7,9 +8,12 @@ name = doc.getElementsByTagName("name")[0]
 print(name.firstChild.data)
 
 staffs = doc.getElementsByTagName("staff")
+#todas las etiquetas llamadas staff las ponemos en un arreglo para luego guardarlas
+# y luego imprimirlas.
 for staff in staffs:
         sid = staff.getAttribute("id")
         nickname = staff.getElementsByTagName("nickname")[0]
         salary = staff.getElementsByTagName("salary")[0]
         print("id:%s, nickname:%s, salary:%s" %
               (sid, nickname.firstChild.data, salary.firstChild.data))
+
